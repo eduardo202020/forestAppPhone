fdbb1939aef44e21e7556ac687b1cb8e3aaa00a3
Jhunior <jguevaral@uni.pe>
2025-03-13 11:40:42 -0500

    Add react-native-paper and vector-icons; update TabOneScreen layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,13 +2,18 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { Provider as PaperProvider, Button, Text as Text2 } from 'react-native-paper';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text2 variant="titleLarge">Hola Mundo</Text2>
+        <Button mode="contained" onPress={() => console.log('Presionado')}>
+          Presioname
+        </Button>
+      </View>
+
     </View>
   );
 }
