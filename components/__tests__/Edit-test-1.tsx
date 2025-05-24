2c3a04702ae8447fe2b6fe3b8005f6f87a5e7926
PieroValle0302 <piero.valle.z@uni.pe>
2025-05-24 00:03:35 -0500

    Añandiendo los componentes .tsx

---
+++ components/__tests__/Edit-test-1.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+// Un componente de ejemplo para edición
+const Edit = () => <div>Editar proyecto</div>;
+
+describe('Edit', () => {
+  it('renderiza el componente Edit sin errores', () => {
+    const tree = renderer.create(<Edit />).toJSON();
+    expect(tree).toBeTruthy();
+  });
+});
+
+export default Edit;
